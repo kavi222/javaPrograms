b802ed7ac40a4796d36984c43e39da72d3db9183
DELL <DELL@DESKTOP-VV9ES80>
2022-11-24 09:56:10 +0530

    Initial commit: Java Programs

---
+++ JavaProgram/src/strings/reverseStringsWithoutUsingStringInBuiltFns.java
@@ -0,0 +1,15 @@
+package strings;
+
+public class reverseStringsWithoutUsingStringInBuiltFns {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "Automation";
+		StringBuilder str2 = new StringBuilder();
+		str2.append(str);
+		str2=str2.reverse();
+		System.out.println(str2);
+
+	}
+
+}
